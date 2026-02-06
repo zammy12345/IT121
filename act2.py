674bb4c9fddd3679cd2e46e7dfbb75e4c48fe5c4
zammy12345 <zamalucardmagsayo@gmail.com>
2026-02-06 11:19:34 +0800

    Add files via upload

---
+++ act2.py
@@ -0,0 +1,9 @@
+fullname = "Zam S Magsayo"       
+age = 19                   
+height = 174                
+working = False         
+
+print("Fullname:", fullname)
+print("\nAge:", age)
+print("\nHeight", height)
+print("\nCurrently working?", working)
---
+++ okay.py
@@ -0,0 +1,5 @@
+grade = 100
+if grade >= 75:
+    print("passed")
+else:
+    print("failed")
