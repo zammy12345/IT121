5f2870b1cf82b16a4c7cb94712940cd202aad93b
zammy12345 <zamalucardmagsayo@gmail.com>
2026-02-13 10:48:15 +0800

    Add files via upload

---
+++ act4.py
@@ -0,0 +1,9 @@
+print("========Activity 4==========")
+print()
+password = "zam123"
+pas = input("Input Password: ")
+
+if pas == password:
+    print("Login Succesfully")
+else:
+    print("Denied")
